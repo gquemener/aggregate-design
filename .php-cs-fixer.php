26d4148a838b6fee55a59d7032816417e3efc9ab
Gildas Quéméner <gildas.quemener@gmail.com>
2023-07-26 18:01:13 +0200

    declare strict types

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,14 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
 ;
 
 $config = new PhpCsFixer\Config();
 return $config->setRules([
-        '@PSR12' => true,
-        'strict_param' => true,
-        'array_syntax' => ['syntax' => 'short'],
+        '@Symfony:risky' => true,
+        'declare_strict_types' => true,
     ])
     ->setFinder($finder)
     ->setRiskyAllowed(true)
